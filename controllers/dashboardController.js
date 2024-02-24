235d01014408ce8c3ce445315004a02ca7eb223a
sankar mukesh <ysankarmukesh@gmail.com>
2024-02-25 01:51:57 +0530

    f

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -16,7 +16,7 @@ exports.dashboradDetails = async (req, res, next) => {
     const conversations = await Conversation.find({
       members: { $in: [req.payload.user_id] },
     }).populate({ path: "members", select: ["role", "_id"] });
-    const memberIds = new Set();
+    const convsDetails = {}
     let totalConvo = 0;
     let connections_approved = 0;
     let connections_pending = 0;
@@ -27,21 +27,30 @@ exports.dashboradDetails = async (req, res, next) => {
       if (convo.status == "pending") connections_pending += 1;
       totalConvo += 1;
       const otherMembers = convo.members.filter(
-        (v) => v._id !== req.payload.user_id
-      );
-      for (let j = 0; j < otherMembers.length; j++) {
-        const member = otherMembers[j];
-        if (memberIds.has(member._id)) continue;
-        memberIds.add(member._id);
-        convoDetail[member.role] = convoDetail[member.role]
-          ? convoDetail[member.role] + 1
-          : 1;
+        (v) => v._id.toString() !== req.payload.user_id
+      )[0];
+
+      if (!convsDetails[otherMembers.role]) {
+        convsDetails[otherMembers.role] = { "pending": 0, "approved": 0, "userCreatedConv-pending": 0, "userCreatedConv-approved": 0, "userReceivedConv-approved": 0, "userReceivedConv-pending": 0 }
       }
+      if (convo.status == 'pending') {
+        convsDetails[otherMembers.role] = { ...convsDetails[otherMembers.role], "pending": convsDetails[otherMembers.role].pending + 1 }
+      } else {
+        convsDetails[otherMembers.role] = { ...convsDetails[otherMembers.role], "approved": convsDetails[otherMembers.role].approved + 1 }
+      }
+
+      if (req.payload.user_id !== convo.requestedTo.toString()) {
+        convsDetails[otherMembers.role] = { ...convsDetails[otherMembers.role], [`userCreatedConv-${convo.status}`]: convsDetails[otherMembers.role][`userCreatedConv-${convo.status}`] + 1 }
+      } else {
+        convsDetails[otherMembers.role] = { ...convsDetails[otherMembers.role], [`userReceivedConv-${convo.status}`]: convsDetails[otherMembers.role][`userReceivedConv-${convo.status}`] + 1 }
+      }
+
     }
+
     return res.status(200).json({
       total_connections: totalConvo,
       total_pitches: pitches.length,
-      connections: convoDetail,
+      connections: convsDetails,
       pitches: pitchDetail,
       connections_approved,
       connections_pending,
