095e6f53884ea1987c9fb30684a57c2b397ae2f2
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2024-03-04 22:41:29 +0530

    final commmit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
-
+app.use("/hello", (req, res) => res.send("helooo!"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 // ROUTES
 app.use("/api/auth", authRouter);
