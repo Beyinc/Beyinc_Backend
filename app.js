8e0fa76f94046cabbcc00c4122bac2597b0a8c2d
Sahil Gupta <guptas3067@gmail.com>
2025-06-09 15:07:16 +0530

    cors fix

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const morgan = require("morgan");
 
 const app = express();
 // MIDDLEWARES
-app.use(cors());
+app.use(cors({origin:['http://localhost:3000','https://beyinc-frontend.vercel.app']}));
 app.use(morgan("tiny"));
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
