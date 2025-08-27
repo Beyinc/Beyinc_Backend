021a04f489ce74de9f4daad999252e0ab9ae1afb
ankitk2003 <kumarankit0262@gmail.com>
2025-08-27 12:43:15 +0530

    added report type

--- models/Posts.js
+++ models/Posts.js
@@ -1,106 +1,120 @@
 const mongoose = require("mongoose");
-// category date private/public 
+// category date private/public
 const postSchema = new mongoose.Schema(
-    {
-        description: {
-            type: String,
-        },
-        link: {
-            type: String,
-        },
-        visibility: {
-            type: String,
-            enum: ["public", "private"], // Only allows these two values
-            default: "public", // Default to public if not specified
-        },
-        
-        image: {
-            public_id: {
-                type: String,
-            },
-            url: {
-                type: String,
-            },
-        },
-        createdBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
-        type: {
-            type: String,
-            enum: [
-                'Idea Discussion', 
-                'Co-founder Needed', 
-                'Tech Partner Needed', 
-                'Mentor Needed', 
-                'General Post', 
-                'Question and Answer', 
-                'Announcement', 
-                'News', 
-                'Hiring', 
-                'Opportunities', 
-                'Investment'
-            ],
-        },
-        postTitle: {
-            type: String,
-        },
-        fullDetails: {
-            type: String,
-        },
-        groupDiscussion: {
-            type: String,
-        },
-        reported: {
-            type: Boolean,
-        },
-        openDiscussion: {
-            type: Boolean,
-        },
-        openDiscussionTeam: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        }],
-        openDiscussionRequests: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        }],
-        reportBy: [{
-            user: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-            },
-            reportedTime: {
-                type: Date
-            },
-            reason: {
-                type: String
-            },
-        }],
-        
-        
-        tags: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        }],
-        likes: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        }],
-
-        disLikes: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        }],
-
-        pitchId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Pitch'
-        }
+  {
+    description: {
+      type: String,
     },
-    {
-        timestamps: true, // This adds 'createdAt' and 'updatedAt' fields
-    }
+    link: {
+      type: String,
+    },
+    visibility: {
+      type: String,
+      enum: ["public", "private"], // Only allows these two values
+      default: "public", // Default to public if not specified
+    },
+
+    image: {
+      public_id: {
+        type: String,
+      },
+      url: {
+        type: String,
+      },
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      enum: [
+        "Idea Discussion",
+        "Co-founder Needed",
+        "Tech Partner Needed",
+        "Mentor Needed",
+        "General Post",
+        "Question and Answer",
+        "Announcement",
+        "News",
+        "Hiring",
+        "Opportunities",
+        "Investment",
+      ],
+    },
+    postTitle: {
+      type: String,
+    },
+    fullDetails: {
+      type: String,
+    },
+    groupDiscussion: {
+      type: String,
+    },
+    reported: {
+      type: Boolean,
+    },
+    openDiscussion: {
+      type: Boolean,
+    },
+    openDiscussionTeam: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    openDiscussionRequests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    reportBy: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        reportedTime: {
+          type: Date,
+        },
+        reason: {
+          type: String,
+        },
+        reportType: {
+          type: String,
+        },
+      },
+    ],
+
+    tags: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    disLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    pitchId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pitch",
+    },
+  },
+  {
+    timestamps: true, // This adds 'createdAt' and 'updatedAt' fields
+  }
 );
 
 const Posts = new mongoose.model("Posts", postSchema);
