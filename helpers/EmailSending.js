eab0bb6c28d1c3b9ecc0dea107b4084449f7569b
Sahil Gupta <guptas3067@gmail.com>
2025-10-12 13:12:49 +0530

    email logo fix

--- helpers/EmailSending.js
+++ helpers/EmailSending.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { signEmailOTpToken } = require("./jwt_helpers");
 const Userverify = require("../models/OtpModel");
 dotenv.config({ path: "../config.env" });
+
 const send_Notification_mail = async (to, subject, body, userName, fLink, ...args) => {
   try {
     const transporter = nodemailer.createTransport({
@@ -20,10 +21,10 @@ const send_Notification_mail = async (to, subject, body, userName, fLink, ...arg
       subject: subject,
       html: `
             <div style="max-width: 600px; margin: 0 auto; background: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0,0,0,0.1); border-top: 4px solid #6a73fa; border-bottom: 4px solid #6a73fa;">
-            <img src=${process.env.MAIL_LOGO} alt="Email Banner" style="display: block; margin: 0 auto 20px; max-width: 40%; height: auto;">
+            <img src="cid:logo" alt="Email Banner" style="display: block; margin: 0 auto 20px; max-width: 40%; height: auto;">
             <p>Hi, <b>${userName}</b></p>
             <p>${body}</p>
-              <a href = ${process.env.BEYINC_SITE+fLink} style="display: inline-block; padding: 10px 20px; background-color: #6a73fa; color: #fff; text-decoration: none; border-radius: 5px;">Go to BeyInc</a>       
+              <a href = ${process.env.BEYINC_SITE + fLink} style="display: inline-block; padding: 10px 20px; background-color: #6a73fa; color: #fff; text-decoration: none; border-radius: 5px;">Go to BeyInc</a>       
               <p style="margin-top: 20px;">Best Regards,<br><b>BeyInc</b></p>
               <div style="margin-top: 20px; background-color: #f0f0f0; padding: 10px; border-radius: 5px; text-align: center;">
                   <p style="margin: 0;">&copy; Copyright BeyInc</p>
@@ -31,6 +32,13 @@ const send_Notification_mail = async (to, subject, body, userName, fLink, ...arg
             </div>
               </div>
        `,
+      attachments: [
+        {
+          filename: "logo.png", // convert your SVG to PNG first
+          path: __dirname + "/../assets/Bloomr-login-logo.png", // path to the image in your project
+          cid: "logo" // same as used in the HTML
+        }
+      ]
     };
 
     // Send email
