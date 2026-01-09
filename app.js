3df0b0b46831d417a54f7004baa6259577f82ea5
ankitk2003 <kumarankit0262@gmail.com>
2026-01-09 12:25:04 +0530

    cors fixed

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000', 'https://beyinc-frontend.vercel.app', 'https://yellow-mushroom-0aec0e610.2.azurestaticapps.net', 'https://www.bloomr.world','https://beyinc-frontend-dg45.vercel.app'] }));
-app.use(cors());
+// app.use(cors({ origin: ['http://localhost:3000', 'https://beyinc-frontend.vercel.app', 'https://yellow-mushroom-0aec0e610.2.azurestaticapps.net', 'https://www.bloomr.world','https://beyinc-frontend-dg45.vercel.app'] }));
+// app.use(cors());
 const corsOptions = {
   origin: [
     'http://localhost:3000',
