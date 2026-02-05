be3115d5ba69cff922b6a96ef6002d6b141ec573
Beyinc <rishabhjambhulkar@gmail.com>
2026-02-05 17:21:29 +0530

    Fix CORS origin entry for bloomr.world

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const corsOptions = {
     'http://localhost:3000',
     'https://beyinc-frontend.vercel.app',
     'https://yellow-mushroom-0aec0e610.2.azurestaticapps.net',
-'https://www.bloomr.world',
+    'https://www.bloomr.world',
     'https://bloomr.world',
     'https://beyinc-frontend-dg45.vercel.app'
   ],
