d2b0a715c6e4a9e253a11cc6da070f62a21c27fa
ankitk2003 <kumarankit0262@gmail.com>
2026-03-02 15:50:26 +0530

    chat room

--- routes/matchRouter.js
+++ routes/matchRouter.js
@@ -11,7 +11,7 @@ const {
 } = require("../controllers/chatRoom/index");
 const router = express.Router();
 ///
-
+/////
 router.route("/quickMatch").post(quickMatch);
 router.route("/joinRoom").post(joinQuickMatchRoom);
 
